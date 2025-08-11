5cca9101c25af26bc9541f07c221074547b3a2e4
gstdl <54521960+gstdl@users.noreply.github.com>
2025-08-12 01:28:59 +0700

    fix queries

--- changelog/changelog-index.sql
+++ changelog/changelog-index.sql
@@ -1,33 +1,33 @@
 --liquibase formatted sql
 
 --changeset imdb:101-create-directors-name-index
-CREATE INDEX idx_directors_name ON directors(first_name, last_name);
+CREATE INDEX idx_directors_name ON directors (first_name, last_name);
 --rollback DROP INDEX idx_directors_name ON directors;
 
 --changeset imdb:102-create-actors-name-index
-CREATE INDEX idx_actors_name ON actors(first_name, last_name);
+CREATE INDEX idx_actors_name ON actors (first_name, last_name);
 --rollback DROP INDEX idx_actors_name ON actors;
 
 --changeset imdb:103-create-movies-name-index
-CREATE INDEX idx_movies_name ON movies(name);
+CREATE INDEX idx_movies_name ON movies (name);
 --rollback DROP INDEX idx_movies_name ON movies;
 
 --changeset imdb:104-create-movies-year-index
-CREATE INDEX idx_movies_year ON movies(year);
+CREATE INDEX idx_movies_year ON movies (year);
 --rollback DROP INDEX idx_movies_year ON movies;
 
 --changeset imdb:105-create-movies-rank-index
-CREATE INDEX idx_movies_rank ON movies(rank);
+CREATE INDEX idx_movies_rank ON movies (rank);
 --rollback DROP INDEX idx_movies_rank ON movies;
 
 --changeset imdb:106-create-directors-genres-genre-index
-CREATE INDEX idx_directors_genres_genre ON directors_genres(genre);
+CREATE INDEX idx_directors_genres_genre ON directors_genres (genre);
 --rollback DROP INDEX idx_directors_genres_genre ON directors_genres;
 
 --changeset imdb:107-create-movies-genres-genre-index
-CREATE INDEX idx_movies_genres_genre ON movies_genres(genre);
+CREATE INDEX idx_movies_genres_genre ON movies_genres (genre);
 --rollback DROP INDEX idx_movies_genres_genre ON movies_genres;
 
 --changeset imdb:108-create-roles-role-index
-CREATE INDEX idx_roles_role ON roles(role);
+CREATE INDEX idx_roles_role ON roles (role);
 --rollback DROP INDEX idx_roles_role ON roles;
--- changelog/changelog-view.sql
+++ changelog/changelog-view.sql
@@ -2,7 +2,7 @@
 
 --changeset imdb:201-create-movie-details-view
 CREATE VIEW v_movie_details AS
-SELECT 
+SELECT
     m.id,
     m.name,
     m.year,
@@ -10,54 +10,58 @@ SELECT
     GROUP_CONCAT(DISTINCT mg.genre ORDER BY mg.genre SEPARATOR ', ') AS genres,
     COUNT(DISTINCT r.actor_id) AS actor_count,
     COUNT(DISTINCT md.director_id) AS director_count
-FROM movies m
-LEFT JOIN movies_genres mg ON m.id = mg.movie_id
-LEFT JOIN roles r ON m.id = r.movie_id
-LEFT JOIN movies_directors md ON m.id = md.movie_id
+FROM movies AS m
+LEFT JOIN movies_genres AS mg ON m.id = mg.movie_id
+LEFT JOIN roles AS r ON m.id = r.movie_id
+LEFT JOIN movies_directors AS md ON m.id = md.movie_id
 GROUP BY m.id, m.name, m.year, m.rank;
 --rollback DROP VIEW v_movie_details;
 
 --changeset imdb:202-create-director-stats-view
 CREATE VIEW v_director_stats AS
-SELECT 
+SELECT
     d.id,
     CONCAT(d.first_name, ' ', d.last_name) AS full_name,
-    COUNT(DISTINCT md.movie_id) AS movie_count,
-    GROUP_CONCAT(DISTINCT dg.genre ORDER BY dg.genre SEPARATOR ', ') AS preferred_genres,
-    AVG(m.rank) AS avg_movie_rank
-FROM directors d
-LEFT JOIN movies_directors md ON d.id = md.director_id
-LEFT JOIN directors_genres dg ON d.id = dg.director_id
-LEFT JOIN movies m ON md.movie_id = m.id
+    COUNT(DISTINCT md.movie_id)            AS movie_count,
+    GROUP_CONCAT(
+        DISTINCT dg.genre
+        ORDER BY dg.genre
+        SEPARATOR ', '
+    )                                      AS preferred_genres,
+    AVG(m.`rank`)                          AS avg_movie_rank
+FROM directors AS d
+LEFT JOIN movies_directors AS md ON d.id = md.director_id
+LEFT JOIN directors_genres AS dg ON d.id = dg.director_id
+LEFT JOIN movies AS m ON md.movie_id = m.id
 GROUP BY d.id, d.first_name, d.last_name;
 --rollback DROP VIEW v_director_stats;
 
 --changeset imdb:203-create-actor-stats-view
 CREATE VIEW v_actor_stats AS
-SELECT 
+SELECT
     a.id,
-    CONCAT(a.first_name, ' ', a.last_name) AS full_name,
     a.gender,
-    COUNT(DISTINCT r.movie_id) AS movie_count,
-    COUNT(DISTINCT r.role) AS unique_roles_count,
-    AVG(m.rank) AS avg_movie_rank
-FROM actors a
-LEFT JOIN roles r ON a.id = r.actor_id
-LEFT JOIN movies m ON r.movie_id = m.id
+    CONCAT(a.first_name, ' ', a.last_name) AS full_name,
+    COUNT(DISTINCT r.movie_id)             AS movie_count,
+    COUNT(DISTINCT r.role)                 AS unique_roles_count,
+    AVG(m.`rank`)                          AS avg_movie_rank
+FROM actors AS a
+LEFT JOIN roles AS r ON a.id = r.actor_id
+LEFT JOIN movies AS m ON r.movie_id = m.id
 GROUP BY a.id, a.first_name, a.last_name, a.gender;
 --rollback DROP VIEW v_actor_stats;
 
 --changeset imdb:204-create-genre-stats-view
 CREATE VIEW v_genre_stats AS
-SELECT 
+SELECT
     mg.genre,
     COUNT(DISTINCT mg.movie_id) AS movie_count,
-    AVG(m.rank) AS avg_movie_rank,
-    MAX(m.rank) AS best_movie_rank,
-    MIN(m.year) AS earliest_year,
-    MAX(m.year) AS latest_year
-FROM movies_genres mg
-JOIN movies m ON mg.movie_id = m.id
+    AVG(m.`rank`)               AS avg_movie_rank,
+    MAX(m.`rank`)               AS best_movie_rank,
+    MIN(m.`year`)               AS earliest_year,
+    MAX(m.`year`)               AS latest_year
+FROM movies_genres AS mg
+INNER JOIN movies AS m ON mg.movie_id = m.id
 WHERE m.rank IS NOT NULL
 GROUP BY mg.genre
 ORDER BY avg_movie_rank DESC;
